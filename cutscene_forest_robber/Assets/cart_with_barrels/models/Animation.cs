35e1c86dcf05352bfd0db13368d9cd80b458778f
Daniel Gagnon <Cooldan@live.ca>
2020-07-15 17:22:24 -0400

    initial state

---
+++ cutscene_forest_robber/Assets/cart_with_barrels/models/Animation.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartAnimation();
+    }
+
+   void StartAnimation()
+    {
+        this.gameObject.GetComponent<Animator>().SetBool("Roll", true);
+    }
+}
